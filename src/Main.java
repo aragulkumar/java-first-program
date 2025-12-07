63bcb8b6adca76836a01babc310765be5e750961
aragulkumar <aragulkumar11@gmail.com>
2025-12-07 18:35:57 +0530

    Initial commit - connected IntelliJ project to GitHub

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args){
+
+        // THIS IS MY FIRST JAVA PROJECT
+
+        /*
+        HELLO IT IS DEMO MULTILINE COMMAND
+         */
+
+//        System.out.print("I like Bolli! \n");
+//        System.out.print("Its really sweet \n");
+//        System.out.print("Buy me one");
+//        System.out.println();
+
+
+        /*
+        -------------------------------------
+        Java has 2 type of variables
+        - Primitive
+        - Reference
+        ------------------------------------------------
+        Primitive has int, boolean, char, double
+        Reference has string, array, object
+        -------------------------------------------
+        2 Steps to create variables
+        -------------------------------------------
+        - 1) Declaration
+        - 2) Assignment
+
+         */
+        int age=19;
+        System.out.println(age);
+
+        int year=2025;
+        System.out.println("The year is " + year);
+    }
+}
