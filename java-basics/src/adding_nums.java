f0e785ca151fa406e73ed22cf44b17b5eb8fdd11
Ragul Kumar A <aragulkumar11@gmail.com>
2026-05-22 15:32:12 +0000

    Addition of two nums

---
+++ java-basics/src/adding_nums.java
@@ -0,0 +1,11 @@
+public class adding_nums {
+    public static void main(String[] args){
+
+        int x = 5;
+        int y = 6;
+
+        int z = x + y;
+
+        System.out.println("The addition of z and y is " + z);
+    }
+}
