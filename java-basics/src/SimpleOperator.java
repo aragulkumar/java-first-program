197c5198805b83a66604a72f17474381a5d46316
Ragul Kumar A <aragulkumar11@gmail.com>
2026-05-26 16:21:17 +0000

    Java Operators

---
+++ java-basics/src/SimpleOperator.java
@@ -0,0 +1,13 @@
+public class SimpleOperator {
+    public static void main(String[] args){
+        int a = 10, b = 3;
+
+        // arithmetic operation
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+
+        // Relational Operation
+        System.out.println("a > b = " + (a > b));
+
+    }
+}
