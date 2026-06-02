579a69a5608f852d2f2313ea07886d7756838b07
Ragul Kumar A <aragulkumar11@gmail.com>
2026-06-02 17:38:47 +0000

    For Loop & while Loop in java

---
+++ java-basics/src/loops.java
@@ -0,0 +1,21 @@
+public class loops {
+    public static void main(String[] args){
+
+        // for loop
+        System.out.println("For Loop");
+        for (int i=1; i <= 5; i++){
+            System.out.println("Hiii" + i);
+        }
+
+        // While Loop
+
+        System.out.println("While Loop");
+        int j = 1;
+
+        while (j<=5){
+            System.out.println("While Loop" + j);
+            j++;
+        }
+
+    }
+}
