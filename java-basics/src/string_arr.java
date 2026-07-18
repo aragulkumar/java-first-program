167ff98a7f5695811be07a6d938126e363d94938
Ragul Kumar A <aragulkumar11@gmail.com>
2026-07-18 17:47:00 +0000

    Java starting main(string[] args) how it works

---
+++ java-basics/src/string_arr.java
@@ -0,0 +1,7 @@
+public class string_arr {
+    public static void main(String[] args){
+        System.out.println(args[1]);
+        // the string[] creates a array which stores the value when compiling if we give value after it.
+        // eg. java string_arr 30 50 ------- so it prints 50 as output because of it.
+    }
+}
