551573d1e9f97986a6b880cf19a41ede33a3b84f
aragulkumar <aragulkumar11@gmail.com>
2026-08-02 07:39:15 +0530

    Method Overloading & HOw class is stored in heap

--- OOPs/ClassCalculator.java
+++ OOPs/ClassCalculator.java
@@ -1,7 +1,25 @@
 
 class calculator
 {
+    // Here we have three different methods with same name 
+    // But different parameters so this is called
+    // Method Overloading
+
     public int add(int n1, int n2)
+    {
+        int tot = n1 + n2;
+
+        return tot;
+    }
+
+    public int add(int n1, int n2, int n3)
+    {
+        int tot = n1 + n2 + n1;
+
+        return tot;
+    }
+
+    public double add(double n1, int n2)
     {
         int tot = n1 + n2;
 
@@ -20,6 +38,10 @@ public class ClassCalculator
         calculator calc = new calculator();
 
         int result = calc.add(num1,num2);
+
+        // The class created is in a heap and from there we access the method. 
+        // // The heap is stored as memory address in system.
+
 
         System.out.println(result); 
 
