532bc9b5db248c4e28edcb5005f91c6a3a4e013f
aragulkumar <aragulkumar11@gmail.com>
2026-08-04 22:53:20 +0530

    chore: clean up project structure and update .gitignore

---
+++ java-basics/SwitchSyntax.java
@@ -0,0 +1,17 @@
+public class SwitchSyntax {
+    static void main(String[] args) {
+
+        switch ("Expression"){
+
+            // cases
+            case "One" :
+                // do something
+                break;
+            case "Two":
+                // do something
+                break;
+            default:
+                // do something
+        }
+    }
+}
--- java-basics/src/scanner.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class scanner {
-    public static void main(String[] args){
-        Scanner a = new Scanner(System.in);
-        int out = a.nextInt();
-    }
-}
